f6e7ef0d0b4550b8a2866d9f72a041d20cca6db6
dianejdan <dianejdan@users.noreply.github.com>
2014-12-10 14:35:01 -0500

    Update Cluster_Coefficient.java

--- Cluster_Coefficient.java
+++ Cluster_Coefficient.java
@@ -21,9 +21,6 @@ public class Clustering_Coefficient {
         FileInputFormat.addInputPaths(conf1, args[0]);
         FileOutputFormat.setOutputPath(conf1, new Path(args[1]));
 
-        // write other necessary job configuration for example setMapperClass, setReducerClass and so on.
-        // finally run the job,
-        
         conf1.setOutputKeyClass(IntWritable.class); // set output key class
         conf1.setOutputValueClass(DoubleWritable.class); // set output value class
         
