aa2fa44ce06a0cb5857612cd40aa922b3c5222c3
dianejdan <dianejdan@users.noreply.github.com>
2014-12-10 14:34:24 -0500

    Create Cluster_Coefficient.java

---
+++ Cluster_Coefficient.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class Clustering_Coefficient {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 2) {
+            System.err.println("Usage: "+" <inpath> <outpath>");
+            System.exit(1);
+        }
+
+        // first stage mapper reducer to make partition
+        JobConf conf1  = new JobConf(Clustering_Coefficient.class);
+        conf1.setInputFormat(MyInputFormat.class);
+        FileInputFormat.addInputPaths(conf1, args[0]);
+        FileOutputFormat.setOutputPath(conf1, new Path(args[1]));
+
+        // write other necessary job configuration for example setMapperClass, setReducerClass and so on.
+        // finally run the job,
+        
+        conf1.setOutputKeyClass(IntWritable.class); // set output key class
+        conf1.setOutputValueClass(DoubleWritable.class); // set output value class
+        
+        conf1.setMapperClass(mymapper.class); // set mapper
+        conf1.setReducerClass(myreducer.class); // set reducer
+        
+        JobClient.runJob(conf1); // run jobs
+    }
+}
