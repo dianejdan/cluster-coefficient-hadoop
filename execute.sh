7ef4b69356931a1d908359458e8c54684a6f1e1c
dianejdan <dianejdan@users.noreply.github.com>
2014-12-10 14:41:30 -0500

    Create execute.sh

---
+++ execute.sh
@@ -0,0 +1,3 @@
+/home/scratch/hadoop_core/bin/hadoop dfs -rmr output*
+
+/home/scratch/hadoop_core/bin/hadoop jar /home/mygroup/clus-coef.jar Clustering_Coefficient /user/mygroup/input /user/mygroup/output
