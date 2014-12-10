03d28ce038b163ed30b95b67193bcb4cd9ee78ae
dianejdan <dianejdan@users.noreply.github.com>
2014-12-10 14:50:06 -0500

    Update MyInputFormat.java

--- MyInputFormat.java
+++ MyInputFormat.java
@@ -33,67 +33,66 @@ import org.apache.hadoop.util.ToolRunner;
 //public class read_data extends Configured implements Tool {
 
 
-	public class MyInputFormat 
-    extends MultiFileInputFormat<NullWritable, Text>  {
+public class MyInputFormat extends MultiFileInputFormat<NullWritable, Text>  {
 
     @Override
     public RecordReader<NullWritable,Text> getRecordReader(InputSplit split
         , JobConf job, Reporter reporter) throws IOException {
-      return new FilepathRecordReader(job, (MultiFileSplit)split);
+        return new FilepathRecordReader(job, (MultiFileSplit)split);
     }
   
 
-	public static class FilepathRecordReader 
-    implements RecordReader<NullWritable, Text> {
+    public static class FilepathRecordReader implements RecordReader<NullWritable, Text> {
 
-		private MultiFileSplit split;
-   	 	private FileSystem fs;
+	private MultiFileSplit split;
+   	private FileSystem fs;
     	private int count = 0;
     	private Path[] paths;
-		private boolean process = false;
+	private boolean process = false;
         private long offset; //total offset read so far;
         private long totLength;
 
-		public FilepathRecordReader(Configuration conf, MultiFileSplit split)
+	public FilepathRecordReader(Configuration conf, MultiFileSplit split)
       	throws IOException {
       
-      		this.split = split;
-      		fs = FileSystem.get(conf);
-      		this.paths = split.getPaths();
+      	    this.split = split;
+      	    fs = FileSystem.get(conf);
+      	    this.paths = split.getPaths();
     	}
 
-		public void close() throws IOException { }
+	public void close() throws IOException { }
 
-		public long getPos() throws IOException {
-			return process ? 1 : 0;
-		}
-
-		public float getProgress() throws IOException{
-			return process ? 1.0f : 0.0f;
-		}
-
-		public boolean next(NullWritable key, Text value) throws IOException {
-      
-			if(count >= split.getNumPaths())
-		        return false;
-			
-			if(!process)
-			{
-				Path file = paths[count];
-				Text filepath = new Text(file.toString());
-				value.set(filepath);
-				count++;
-				return true;
-			}
-
-			return false;
-		}
-		public Text createValue() {
-      		return new Text();
-    	}
-		public NullWritable createKey()	{
-			return NullWritable.get();
-		}
-
+	public long getPos() throws IOException {
+	    return process ? 1 : 0;
 	}
+
+	public float getProgress() throws IOException{
+	    return process ? 1.0f : 0.0f;
+	}
+
+	public boolean next(NullWritable key, Text value) throws IOException {
+      
+	    if(count >= split.getNumPaths())
+	        return false;
+			
+	    if(!process) {
+		Path file = paths[count];
+		Text filepath = new Text(file.toString());
+		value.set(filepath);
+		count++;
+		return true;
+	    }
+
+	    return false;
+	}
+	
+	public Text createValue() {
+      	    return new Text();
+    	}
+	
+	public NullWritable createKey()	{
+	    return NullWritable.get();
+	}
+
+    }
 }
